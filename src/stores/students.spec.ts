d6f1789599976dc0363837a1b124b87dd4234c77
Lucas Borges <lucasvnborges@gmail.com>
2024-05-02 02:08:02 -0300

    feat: ajustar os testes de acordo com a integração com os services de estudantes

--- src/stores/students.spec.ts
+++ src/stores/students.spec.ts
@@ -1,11 +1,28 @@
-import { describe, test, expect, beforeEach } from 'vitest'
+import { setupServer } from 'msw/node'
+import { handlers } from '@/mocks/handlers'
+import { describe, test, expect, beforeEach, beforeAll, afterAll } from 'vitest'
 import { setActivePinia, createPinia } from 'pinia'
 import { useStudentStore } from './students'
 import { mockStudentData } from '@/mocks/data'
 
-describe('Store de estudantes', () => {
+const server = setupServer(...handlers)
+
+beforeAll(() => {
+  server.listen()
+})
+
+afterAll(() => {
+  server.close()
+})
+
+describe('Gerenciamento de estado dos estudantes', () => {
   beforeEach(() => {
     setActivePinia(createPinia())
+  })
+
+  test('Espera que a lista de estudantes seja um objeto', () => {
+    const { students } = useStudentStore()
+    expect(students).toBeTypeOf('object')
   })
 
   test('Espera que a lista de estudantes esteja vazia', () => {
@@ -13,45 +30,48 @@ describe('Store de estudantes', () => {
     expect(students.length).toBe(0)
   })
 
-  test('Espera que a lista de estudantes contenha um novo aluno', () => {
+  test('Espera que a lista de estudantes contenha um novo aluno', async () => {
     const { students, create } = useStudentStore()
-    create(mockStudentData)
+    await create(mockStudentData)
     expect(students.length).toBe(1)
   })
 
-  test('Espera que a busca por id encontre um novo aluno', () => {
+  test('Espera que a busca por id encontre um novo aluno', async () => {
     const { create, findById } = useStudentStore()
-    const newStudent = create(mockStudentData)
-    const foundStudent = findById(newStudent.id)
+    const newStudent = await create(mockStudentData)
 
-    if (foundStudent !== undefined) {
-      expect(foundStudent.id).toBe(newStudent.id)
-    }
+    let foundStudent
+    if (newStudent.id) foundStudent = findById(newStudent.id)
+
+    if (foundStudent !== undefined) expect(foundStudent.id).toBe(newStudent.id)
   })
 
-  test('Espera que os dados de um aluno sejam atualizados corretamente', () => {
+  test('Espera que os dados de um aluno sejam atualizados corretamente', async () => {
     const { create, findById, update } = useStudentStore()
-    const newStudent = create(mockStudentData)
+    const newStudent = await create(mockStudentData)
     const modifiedData = { ...newStudent, birthdate: '20-05-1995' }
-    const updateStudent = update(modifiedData)
+    const updateStudent = await update(modifiedData)
 
     expect(updateStudent).toBeTruthy()
-    const foundStudent = findById(newStudent.id)
 
-    if (foundStudent !== undefined) {
-      expect(foundStudent.birthdate).toBe(modifiedData.birthdate)
-    }
+    let foundStudent
+    if (newStudent.id) foundStudent = findById(newStudent.id)
+
+    if (foundStudent !== undefined) expect(foundStudent.birthdate).toBe(modifiedData.birthdate)
   })
 
-  test('Espera excluir um aluno da lista corretamente', () => {
+  test('Espera excluir um aluno da lista corretamente', async () => {
     const { create, findById, exclude } = useStudentStore()
-    const newStudent = create(mockStudentData)
-    const foundStudent = findById(newStudent.id)
+    const newStudent = await create(mockStudentData)
 
-    if (foundStudent !== undefined) {
-      expect(foundStudent.id).toBe(newStudent.id)
+    let foundStudent
+    if (newStudent.id) foundStudent = findById(newStudent.id)
+
+    if (foundStudent !== undefined) expect(foundStudent.id).toBe(newStudent.id)
+
+    if (newStudent.id) {
+      const excludeStudent = exclude(newStudent.id)
+      expect(excludeStudent).toBeTruthy()
     }
-    const excludeStudent = exclude(newStudent.id)
-    expect(excludeStudent).toBeTruthy()
   })
 })
