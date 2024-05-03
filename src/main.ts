e0f3eb9c80e83c3549fb0ad63e85e15534e5f150
Lucas Borges <lucasvnborges@gmail.com>
2024-05-03 13:57:23 -0300

    feat: adicionar toastify para exibir notificações na tela

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import './assets/main.css'
-
+import 'vue3-toastify/dist/index.css'
+// @ts-ignore
+import config from '../formkit.config'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { plugin, defaultConfig } from '@formkit/vue'
 import App from './App.vue'
 import router from './router'
-// @ts-ignore
-import config from '../formkit.config'
+import Vue3Toasity, { type ToastContainerOptions } from 'vue3-toastify'
 
 async function enableMocking() {
   if (process.env.NODE_ENV !== 'development') {
@@ -23,5 +24,8 @@ enableMocking().then(() => {
   app.use(createPinia())
   app.use(router)
   app.use(plugin, defaultConfig(config))
+  app.use(Vue3Toasity, {
+    autoClose: 3000
+  } as ToastContainerOptions)
   app.mount('#app')
 })
