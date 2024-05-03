06ba61e2e515fbe58672153f7045465be97d0f65
Lucas Borges <lucasvnborges@gmail.com>
2024-05-03 14:08:25 -0300

    feat: adicionar condições nos componentes para melhorar as interações e evitar erros

--- src/views/StudentList.vue
+++ src/views/StudentList.vue
@@ -1,19 +1,28 @@
 <template>
   <div class="container md:mx-auto">
     <div class="max-w-4xl md:mx-auto overflow-x-auto mt-12">
-      <div class="flex flex-row justify-between items-center">
-        <h5 class="text-xl font-bold dark:text-white ml-4">
-          Lista de alunos cadastrados
-        </h5>
+      <div class="flex flex-row justify-between items-center mb-6">
+        <h4 class="text-2xl font-bold">Lista de alunos cadastrados</h4>
         <button
           type="button"
-          @click="handleNavigate"
-          class="text-white mt-6 bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 mb-6"
+          @click="navigateToRegister"
+          class="text-gray-900 bg-white border border-gray-300 font-medium rounded-lg text-sm px-5 py-2.5"
         >
           Cadastrar aluno
         </button>
       </div>
+
+      <div
+        role="alert"
+        v-if="students.length === 0"
+        class="p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50"
+      >
+        Comece cadastrando um novo aluno clicando no
+        <span class="font-medium">botão acima ☝️</span>
+      </div>
+
       <table
+        v-if="students.length > 0"
         class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"
       >
         <thead
@@ -27,6 +36,7 @@
             <th scope="col" class="px-6 py-3"></th>
           </tr>
         </thead>
+
         <tbody v-for="(student, index) in students" :key="index">
           <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
             <th
@@ -40,12 +50,15 @@
             <td class="px-6 py-4">{{ student.enrollmentPeriod }}</td>
             <td class="px-6 py-4 flex flex-row justify-between items-center">
               <a
-                @click="exclude(student.id ?? '')"
+                v-if="student.id"
+                @click="exclude(student.id)"
                 class="font-medium text-red-400 dark:text-red-500 hover:underline cursor-pointer mr-2"
               >
                 Excluir
               </a>
               <a
+                v-if="student.id"
+                @click="navigateToUpdate(student.id)"
                 class="font-medium text-blue-600 dark:text-blue-500 hover:underline cursor-pointer"
               >
                 Editar
@@ -64,17 +77,22 @@ import { useStudentStore } from '@/stores/students'
 import { useRouter } from 'vue-router'
 
 const { push } = useRouter()
-const { students, fetchAll, reset, exclude } = useStudentStore()
+const { students, fetchAll, $reset, exclude } = useStudentStore()
 
 onBeforeMount(() => {
+  $reset()
   fetchAll()
 })
 
 onUnmounted(() => {
-  reset()
+  $reset()
 })
 
-function handleNavigate() {
+function navigateToRegister() {
   push('/cadastrar')
+}
+
+function navigateToUpdate(id: string) {
+  push(`/atualizar/${id}`)
 }
 </script>
