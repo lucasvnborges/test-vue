c15251035003e7dac671fe65ad7a889b22b7c577
Lucas Borges <lucasvnborges@gmail.com>
2024-05-05 12:04:59 -0300

    feat: ajustar validação de datas do schema de estudantes

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,16 +1,18 @@
 import { z } from 'zod'
 
+const dateRegex = /^\d{4}-\d{2}-\d{2}$/
+
 export const StudentSchema = z.object({
   name: z
-    .string({ required_error: 'O nome completo é obrigatório' })
+    .string()
     .min(5, { message: 'O nome completo deve ter no mínimo 5 caracteres' }),
   motherName: z
-    .string({ required_error: 'O nome da mãe é obrigatório' })
+    .string()
     .min(5, { message: 'O nome da mãe deve ter no mínimo 5 caracteres' }),
-  birthdate: z.string({
-    required_error: 'A data de nascimento do aluno é obrigatória'
+  birthdate: z.string().refine((value) => dateRegex.test(value), {
+    message: 'Informe uma data de nascimento válida'
   }),
-  enrollmentPeriod: z.string({
-    required_error: 'A data de ingresso na instituição é obrigatória'
+  enrollmentPeriod: z.string().refine((value) => dateRegex.test(value), {
+    message: 'Informe uma data de ingresso válida'
   })
 })
