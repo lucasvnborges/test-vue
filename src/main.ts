fb030318820036ed8a14ae3480811127b2678cbd
Lucas Borges <lucasvnborges@gmail.com>
2024-05-05 15:10:04 -0300

    feat: criar pasta config e ajustar os imports de arquivos de configuração

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import './assets/main.css'
 import 'vue3-toastify/dist/index.css'
-// @ts-ignore
-import config from '../formkit.config'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { plugin, defaultConfig } from '@formkit/vue'
 import App from './App.vue'
 import router from './router'
+import config from './config/formkit.config'
 import Vue3Toasity, { type ToastContainerOptions } from 'vue3-toastify'
 
 async function enableMocking() {
