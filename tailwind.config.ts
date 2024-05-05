8bf15b42bf22130c381b9ead5ccdfc130df6c3e0
Lucas Borges <lucasvnborges@gmail.com>
2024-05-05 15:05:09 -0300

    chore: ajustar configuração do tailwind para o novo formato

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  content: ['./node_modules/flowbite/**/*.js'],
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+    './node_modules/flowbite/**/*.js'
+  ],
   theme: {
     extend: {}
   },
