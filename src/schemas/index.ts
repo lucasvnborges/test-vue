50e93c561b39d50d42ad25721a11032af3b64fe9
Lucas Borges <lucasvnborges@gmail.com>
2024-05-05 16:51:17 -0300

    feat: adicionar validação para campos obrigatórios do formulário

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -4,15 +4,19 @@ const dateRegex = /^\d{4}-\d{2}-\d{2}$/
 
 export const StudentSchema = z.object({
   name: z
-    .string()
+    .string({ required_error: 'O campo deve ser preenchido' })
     .min(5, { message: 'O nome completo deve ter no mínimo 5 caracteres' }),
   motherName: z
-    .string()
+    .string({ required_error: 'O campo deve ser preenchido' })
     .min(5, { message: 'O nome da mãe deve ter no mínimo 5 caracteres' }),
-  birthdate: z.string().refine((value) => dateRegex.test(value), {
-    message: 'Informe uma data de nascimento válida'
-  }),
-  enrollmentPeriod: z.string().refine((value) => dateRegex.test(value), {
-    message: 'Informe uma data de ingresso válida'
-  })
+  birthdate: z
+    .string({ required_error: 'O campo deve ser preenchido' })
+    .refine((value) => dateRegex.test(value), {
+      message: 'Informe uma data de nascimento válida'
+    }),
+  enrollmentPeriod: z
+    .string({ required_error: 'O campo deve ser preenchido' })
+    .refine((value) => dateRegex.test(value), {
+      message: 'Informe uma data de ingresso válida'
+    })
 })
