7536224cdbff5bbaf4d2464e14a20ab0edd69aef
Lucas Borges <lucasvnborges@gmail.com>
2024-05-05 14:49:32 -0300

    style: melhorar organização do código da listagem de estudantes

--- src/views/StudentList.vue
+++ src/views/StudentList.vue
@@ -2,11 +2,12 @@
   <div class="container md:mx-auto">
     <div class="max-w-4xl md:mx-auto overflow-x-auto mt-12">
       <div class="flex flex-row justify-between items-center mb-6">
-        <h4 class="text-2xl font-bold">Lista de alunos cadastrados</h4>
+        <h4 class="font-bold text-2xl">Lista de alunos cadastrados</h4>
+
         <button
           type="button"
           @click="navigateToRegister"
-          class="text-gray-900 bg-white border border-gray-300 font-medium rounded-lg text-sm px-5 py-2.5"
+          class="font-medium text-gray-900 bg-white border border-gray-300 rounded-lg text-sm px-5 py-2.5"
         >
           Cadastrar aluno
         </button>
@@ -15,7 +16,7 @@
       <div
         role="alert"
         v-if="students.length === 0"
-        class="p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50"
+        class="text-sm text-blue-800 rounded-lg bg-blue-50 p-4 mb-4"
       >
         Comece cadastrando um novo aluno clicando no
         <span class="font-medium">botão acima ☝️</span>
@@ -23,11 +24,9 @@
 
       <table
         v-if="students.length > 0"
-        class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"
+        class="text-sm text-left text-gray-500 w-full"
       >
-        <thead
-          class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
-        >
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50">
           <tr>
             <th scope="col" class="px-6 py-3">Nome</th>
             <th scope="col" class="px-6 py-3">Nome da mãe</th>
@@ -38,10 +37,10 @@
         </thead>
 
         <tbody v-for="(student, index) in students" :key="index">
-          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+          <tr class="bg-white border-b">
             <th
               scope="row"
-              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              class="font-medium text-gray-900 whitespace-nowrap px-6 py-4"
             >
               {{ student.name }}
             </th>
@@ -52,14 +51,14 @@
               <a
                 v-if="student.id"
                 @click="exclude(student.id)"
-                class="font-medium text-red-400 dark:text-red-500 hover:underline cursor-pointer mr-2"
+                class="font-medium text-red-400 hover:underline cursor-pointer mr-2"
               >
                 Excluir
               </a>
               <a
                 v-if="student.id"
                 @click="navigateToUpdate(student.id)"
-                class="font-medium text-blue-600 dark:text-blue-500 hover:underline cursor-pointer"
+                class="font-medium text-blue-600 hover:underline cursor-pointer"
               >
                 Editar
               </a>
@@ -77,15 +76,15 @@ import { useStudentStore } from '@/stores/students'
 import { useRouter } from 'vue-router'
 
 const { push } = useRouter()
-const { students, fetchAll, $reset, exclude } = useStudentStore()
+const { students, fetchAll, exclude, resetStore } = useStudentStore()
 
 onBeforeMount(() => {
-  $reset()
+  resetStore()
   fetchAll()
 })
 
 onUnmounted(() => {
-  $reset()
+  resetStore()
 })
 
 function navigateToRegister() {
